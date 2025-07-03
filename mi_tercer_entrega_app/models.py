bedc788f83658a744e05fdaed5829bc239ae2934
Ignacio Cassani <njicassani@gmail.com>
2025-07-03 19:22:48 -0300

    vinculando github primera vez

---
+++ mi_tercer_entrega_app/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+
+# Create your models here.
+
+class Familiar(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    edad = models.IntegerField()
+    fecha_nacimiento = models.DateField()
+    parentesco = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.nombre} {self.apellido}"
+
+
+class Curso(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField(blank=True, null=True)
+    duracion_semanas = models.IntegerField(default=4)
+    fecha_inicio = models.DateField()
+    activo = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.nombre
+
+
+class Estudiante(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    edad = models.IntegerField()
+    fecha_inscripcion = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.nombre} {self.apellido}"
---
+++ mi_tercer_entrega_app/views.py
@@ -0,0 +1,86 @@
+from django.shortcuts import render, redirect
+
+from .models import Familiar, Curso, Estudiante
+
+from .forms import CursoForm, EstudianteForm
+
+# Create your views here.
+from django.http import HttpResponse
+
+
+def inicio(request):
+    return render(request, 'mi_tercer_entrega_app/inicio.html')
+
+
+def saludo(request):
+    return HttpResponse("¡Hola, mundo!")
+
+
+def saludo_con_template(request):
+    return render(request, 'mi_tercer_entrega_app/saludo.html')
+
+
+def crear_familiar(request, nombre):
+    if nombre is not None:
+        # Creamos un nuevo objeto Familiar
+        nuevo_familiar = Familiar(
+            nombre=nombre,
+            apellido="ApellidoEjemplo",
+            edad=30,
+            fecha_nacimiento="1993-01-01",
+            parentesco="Primo"
+        )
+        nuevo_familiar.save()
+    return render(request, "mi_tercer_entrega_app/crear_familiar.html", {"nombre": nombre})
+
+
+def crear_curso(request):
+
+    if request.method == 'POST':
+        form = CursoForm(request.POST)
+        if form.is_valid():
+            # Procesar el formulario y guardar el curso
+            nuevo_curso = Curso(
+                nombre=form.cleaned_data['nombre'],
+                descripcion=form.cleaned_data['descripcion'],
+                duracion_semanas=form.cleaned_data['duracion_semanas'],
+                fecha_inicio=form.cleaned_data['fecha_inicio'],
+                activo=form.cleaned_data['activo']
+            )
+            nuevo_curso.save()
+            return redirect('cursos')
+    else:
+        form = CursoForm()
+        return render(request, 'mi_tercer_entrega_app/crear_curso.html', {'form': form})
+
+
+def crear_estudiante(request):
+
+    if request.method == 'POST':
+        form = EstudianteForm(request.POST)
+        if form.is_valid():
+            # Procesar el formulario y guardar el curso
+            nuevo_curso = Estudiante(
+                nombre=form.cleaned_data['nombre'],
+                apellido=form.cleaned_data['apellido'],
+                email=form.cleaned_data['email'],
+                edad=form.cleaned_data['edad'],
+                fecha_inscripcion=form.cleaned_data['fecha_inscripcion']
+            )
+            nuevo_curso.save()
+            return redirect('inicio')
+    else:
+        form = EstudianteForm()
+        return render(request, 'mi_tercer_entrega_app/crear_estudiante.html', {'form': form})
+
+
+def cursos(request):
+    cursos = Curso.objects.all()
+    return render(request, 'mi_tercer_entrega_app/cursos.html', {'cursos': cursos})
+
+
+def buscar_cursos(request):
+    if request.method == 'GET':
+        nombre = request.GET.get('nombre', '')
+        cursos = Curso.objects.filter(nombre__icontains=nombre)
+        return render(request, 'mi_tercer_entrega_app/cursos.html', {'cursos': cursos, 'nombre': nombre})
